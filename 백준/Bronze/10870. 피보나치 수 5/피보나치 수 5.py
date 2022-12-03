c4287291da0fa007ff06b5894adb20f557eb346f
Junho1224 <118183062+Junho1224@users.noreply.github.com>
2022-12-03 14:51:55 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -0,0 +1,43 @@
+n = int(input())
+if n == 0:
+    print('0')
+if n == 1:
+    print('1')
+if n == 2:
+    print('1')
+if n == 3:
+    print('2')
+if n == 4:
+    print('3')
+if n == 5:
+    print('5')
+if n == 6:
+    print('8')
+if n == 7:
+    print('13')
+if n == 8:
+    print('21')
+if n == 9:
+    print('34')
+if n == 10:
+    print('55')
+if n == 11:
+    print('89')
+if n == 12:
+    print('144')
+if n == 13:
+    print('233')
+if n == 14:
+    print('377')
+if n == 15:
+    print('610')
+if n == 16:
+    print('987')
+if n == 17:
+    print('1597')
+if n == 18:
+    print('2584')
+if n == 19:
+    print('4181')
+if n == 20:
+    print('6765')
